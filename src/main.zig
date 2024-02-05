d101faafa6b622ccede3b47ca1aae0b9555efac5
Theo ZERIBI <tzeribi@gmail.com>
2024-02-05 22:04:43 +0100

    feat(api): Add routes

--- src/main.zig
+++ src/main.zig
@@ -1,60 +1,63 @@
 const std = @import("std");
 const zap = @import("zap");
 
-fn on_request(r: zap.Request) void {
-    const m = "";
-    const p = r.path orelse "/";
-    const qm = if (r.query) |_| "?" else "";
-    const qq = r.query orelse "";
-
-    // curl -H "special-header: hello" http://localhost:3000
-    if (r.getHeader("special-header")) |clstr| {
-        std.debug.print(">> Special Header: {s}\n", .{clstr});
-    } else {
-        std.debug.print(">> Special Header: <unknown>\n", .{});
-    }
-    std.debug.print(">> {s} {s}{s}{s}\n", .{ m, p, qm, qq });
-
-    if (r.body) |the_body| {
-        std.debug.print(">> BODY: {s}\n", .{the_body});
-    }
-
-    r.setContentTypeFromPath() catch return;
-    r.sendBody(
-        \\ <html><body>
-        \\   <h1>Hello from ZAP!!!</h1>
-        \\   <form action="/" method="post">
-        \\     <label for="fname">First name:</label><br>
-        \\     <input type="text" id="fname" name="fname"><br>
-        \\     <label for="lname">Last name:</label><br>
-        \\     <input type="text" id="lname" name="lname">
-        \\     <button>Send</button>
-        \\   </form>
-        \\ </body></html>
-    ) catch return;
+fn dispatch_routes(r: zap.Request) void {
+	if (r.path) |the_path| {
+		if (routes.get(the_path)) |render| {
+			render(r);
+			return;
+		}
+	}
+	r.sendBody("Hello from ZAP!") catch return;
 }
 
+fn static_site(r: zap.Request) void {
+	r.sendBody("<html><body><h1>Hello from STATIC ZAP!</h1></body></html>") catch return;
+}
+
+var dynamic_counter: i32 = 0;
+fn dynamic_site(r: zap.Request) void {
+	dynamic_counter += 1;
+	var buf: [128]u8 = undefined;
+	const filled_buf = std.fmt.bufPrintZ(
+		&buf,
+		"<html><body><h1>Hello # {d} from DYNAMIC ZAP!!!</h1></body></html>",
+		.{dynamic_counter},
+	) catch "ERROR";
+	r.sendBody(filled_buf) catch return;
+}
+
+fn setup_routes(a: std.mem.Allocator) !void {
+	routes = std.StringHashMap(zap.HttpRequestFn).init(a);
+	try routes.put("/static", static_site);
+	try routes.put("/dynamic", dynamic_site);
+}
+
+var routes: std.StringHashMap(zap.HttpRequestFn) = undefined;
+
 pub fn main() !void {
-    var listener = zap.HttpListener.init(.{
-        .port = 3000,
-        .on_request = on_request,
-        .log = false,
-    });
-    try listener.listen();
+	try setup_routes(std.heap.page_allocator);
+	var listener = zap.HttpListener.init(.{
+		.port = 3000,
+		.on_request = dispatch_routes,
+		.log = true,
+		.max_clients = 100000,
+	});
+	try listener.listen();
 
-    std.debug.print("Listening on 0.0.0.0:3000\n", .{});
+	std.debug.print("Listening on 0.0.0.0:3000\n", .{});
 
-    // start worker threads
-    zap.start(.{
-        .threads = 2,
-        .workers = 1,
-    });
+	// // start worker threads
+	zap.start(.{
+		.threads = 2,
+		.workers = 2,
+	});
 }
 
 test "simple test" {
-    std.debug.print("Running simple test\n", .{});
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+	std.debug.print("Running simple test\n", .{});
+	var list = std.ArrayList(i32).init(std.testing.allocator);
+	defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+	try list.append(42);
+	try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
